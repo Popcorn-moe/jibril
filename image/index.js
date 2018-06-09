a7c8900ad90328bb2ad75c3507feb9942bd12710
SkyBeastMC <skybeastmc@gmail.com>
2018-06-09 15:39:09 +0200

    Image module

---
+++ image/index.js
@@ -0,0 +1,65 @@
+import { configurable, RichEmbed, sendDiscordError } from '@popcorn.moe/migi'
+import * as configuration from './defaultConfig.json'
+
+const COMMAND_MATCH = '^$command(?: <@!?(\\d+)>)?'
+const randomIn = array => array[Math.floor(Math.random(array.length))]
+
+@configurable('image', configuration)
+export default class Image {
+	constructor(migi, { category, commands, usage }) {
+		this.migi = migi
+
+		this.category = category
+		this.commands = commands
+		this.usage = usage
+
+		process.nextTick(() => this.setup())
+	}
+
+	setup() {
+		Object.entries(this.commands).forEach(([name, cmd]) =>
+			this.setupOne(name, cmd)
+		)
+	}
+
+	setupOne(name, { desc, msg, gifs }) {
+		const regex = new RegExp(COMMAND_MATCH.replace('$command', name), 'i')
+
+		this[name] = (message, mention) => {
+			const { member, guild } = message
+
+			return Promise.all([
+				message.delete(),
+				this.response(
+					message,
+					msg,
+					gifs,
+					mention ? message.member : message.guild.me,
+					mention ? message.guild.members.get(mention) : message.member
+				)
+			])
+		}
+
+		this.migi.command(regex, this, name, {
+			name,
+			desc,
+			usage: this.usage
+		})
+	}
+
+	response(message, msg, gifs, from, to) {
+		if (!to)
+			return sendDiscordError(message.channel, 'Aucun utilisateur trouvé 😭')
+
+		const send = msg
+			.replace('{0}', from.displayName)
+			.replace('{1}', to.displayName)
+
+		const embed = new RichEmbed()
+			.setTitle(send)
+			.setColor(0x00ae86)
+			.setImage(randomIn(gifs))
+
+		return message.channel.send({ embed })
+	}
+}
